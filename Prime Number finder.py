51fe5033fb0d5a5483af7099a11ecd12726a50c2
Mitchell Young <mitchellyoung@vagelos-ve702-0096.apng.wlan.private.upenn.edu>
2018-01-27 10:14:24 -0500

    Initial work on project

---
+++ Prime Number finder.py
@@ -0,0 +1,9 @@
+#Prime number finder
+import math as math
+
+print 'Welcome to the prime number finder!  To start, input two positive numbers.  We will return any prime numbers between  (and including) the range.'
+
+x1=raw_input('Smallest number to check:')
+x2=raw_input('Largest number to check:')
+
+print x1, x2
