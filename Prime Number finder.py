dcadea98f467478d1c10a027d8fbca1a0ed66f50
Mitchell Young <mitchellyoung@Mitchells-MacBook-Air.local>
2018-01-29 17:23:38 -0500

    Start on new version

--- Prime Number finder.py
+++ Prime Number finder.py
@@ -1,9 +1,12 @@
-#Prime number finder
-import math as math
+#Logic to solving problem:
+#input two numbers, check each interger between x1 and x2 to see if they are devisable by any numbers between 2 and sqrt(x2)
 
 print 'Welcome to the prime number finder!  To start, input two positive numbers.  We will return any prime numbers between  (and including) the range.'
 
 x1=raw_input('Smallest number to check:')
 x2=raw_input('Largest number to check:')
 
-print x1, x2
+#defining final print group
+primes=[]
+
+print primes
