7c7b535fdc1a12bba7fdf930d6aef065b29efe96
Mitchell Young <mitchellyoung@Mitchells-MacBook-Air.local>
2018-01-30 10:37:26 -0500

    Indent/syntax errors

--- Prime Number finder.py
+++ Prime Number finder.py
@@ -1,6 +1,6 @@
 #Logic to solving problem:
 #input two numbers, check each interger between x1 and x2 to see if they are devisable by any numbers between 2 and sqrt(x2)
-
+import math as ma
 print 'Welcome to the prime number finder!  To start, input two positive numbers.  We will return any prime numbers between  (and including) the range.'
 
 x1=raw_input('Smallest number to check:')
@@ -8,5 +8,24 @@ x2=raw_input('Largest number to check:')
 
 #defining final print group
 primes=[]
+
+#defining dividend and low/high values
+x=2
+low=x1
+high=x2
+
+#loop statement
+for x1 in range(low,high):
+    if condition:x1%x==0
+        pass
+            if x1=high:
+                pass break
+                else x1=x1+1
+        else if x=int.ma.sqrt(high)+1:
+                pass
+                    if x1=high:
+                        pass x1=primes, break
+                        else x=x+1
+
 
 print primes
