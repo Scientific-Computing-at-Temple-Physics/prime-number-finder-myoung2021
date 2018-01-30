47214d61b2d4528fc33fc51238fc53b92ef883f9
Mitchell Young <mitchellyoung@Mitchells-MacBook-Air.local>
2018-01-30 15:01:29 -0500

    Fin.

--- Prime Number finder.py
+++ Prime Number finder.py
@@ -1,29 +1,33 @@
-#Logic to solving problem:
-#input two numbers, check each interger between x1 and x2 to see if they are devisable by any numbers between 2 and sqrt(x2)
+#Mitchell Young, with bugfixing help by Joseph Grassi
 import math as ma
-print 'Welcome to the prime number finder!  To start, input two positive numbers.  We will return any prime numbers between  (and including) the range.'
+print 'Welcome to the prime number finder!  To start, input two positive numbers.  We will return any prime numbers between (and including) the range.'
 
-x1=raw_input('Smallest number to check:')
-x2=raw_input('Largest number to check:')
+x1=int(raw_input('Smallest number to check:'))
+x2=int(raw_input('Largest number to check:'))
 
 #defining final print group
 primes=[]
 
-#defining dividend/divisor and low/high values
-divisor=2
-dividend=x1
-
+#defining low/high values
 low=x1
 high=x2
-
+#BUGFIX: print type(primes)
 #loop statement
 for dividend in range(low,high):
-    for divisor in range(2,int(ma.sqrt(max)+1)):
+    for divisor in range(2,int(ma.ceil(ma.sqrt(dividend)+1))):
         if dividend%divisor ==0:
-            dividend=dividend+1
-        elif divisor==int(ma.sqrt(max)+1
-            primes=primes+dividend and dividend=dividend+1
-        else divisor=divisor+1 and dividend=dividend+1
+            break
+        elif divisor==int(ma.ceil(ma.sqrt(dividend))):
+            #BUGFIX: print "got to line 22"
+            primes.append(int(dividend))
+if x1>=x2:
+    print "Max value must be larger than min value."
+else:
+    if len(primes)==0:
+        print "No Primes Found"
+    else:
+        print primes
+        print "There are",len(primes),"primes between", x1,"and", x2
 
 
 """    if x1%x=0
@@ -36,6 +40,5 @@ for dividend in range(low,high):
             primes=primes+x1, break
         else:
             x=x+1
+ and dividend=dividend+1
 """
-
-print primes
